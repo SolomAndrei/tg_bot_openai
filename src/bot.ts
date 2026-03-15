61584b561099ada8f8307260d8a9505b7f60d17c
utimur <akva_ten@mail.ru>
2026-03-15 14:36:15 +0300

    template

--- src/bot.ts
+++ src/bot.ts
@@ -1,11 +1,6 @@
 import "dotenv/config";
 import { Bot } from "grammy";
-import { BotContext, SessionData } from "./types/bot-types";
-import { startHandler } from "./handlers/start";
-import { AiAnswerHandler } from "./handlers/ai-answer";
-import { Hears } from "./consts/hears";
-import { HelpHandler } from "./handlers/help";
-import { session } from "grammy";
+import { BotContext } from "./types/bot-types";
 
 const BOT_TOKEN = process.env.BOT_TOKEN;
 
@@ -15,21 +10,3 @@ if (!BOT_TOKEN) {
 
 export const bot = new Bot<BotContext>(BOT_TOKEN);
 
-bot.use(
-  session<SessionData, BotContext>({
-    initial: () => ({
-      waitingForAI: false,
-    }),
-  })
-);
-
-bot.command("start", startHandler);
-
-bot.hears(Hears.AI_HELPER, (ctx, next) => {
-  ctx.session.waitingForAI = true;
-  ctx.reply("Задайте ваш вопрос:");
-});
-bot.hears(Hears.TEST_GENERATOR, AiAnswerHandler);
-bot.hears(Hears.HELP, HelpHandler);
-
-bot.on("message:text", AiAnswerHandler);
